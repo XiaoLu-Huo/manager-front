0353653ff33e5ecaa0fbd000b6b29371117e3718
xiaolu.huo <xiaolu.huo@thoughtworks.com>
2023-07-10 17:47:52 +0800

    [feat] login error trun to login page

--- front-manager/src/utils/request.ts
+++ front-manager/src/utils/request.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import {getToken} from "@/utils/token";
+import router from "@/router";
+
 const request = axios.create({
     baseURL: 'http://localhost:8080',
     timeout: 30000
@@ -7,7 +9,8 @@ const request = axios.create({
 
 request.interceptors.request.use(config => {
     config.headers['content-type'] = 'application/json;charset=utf-8'
-    config.headers['authority'] = 'Bearer '+ getToken()
+    // config.headers['authority'] = 'Bearer '+ getToken()
+    config.headers['token'] = getToken()
     return config
 }, error => {
     console.log('request error: ' + error) // for error debug
@@ -20,6 +23,8 @@ request.interceptors.response.use(response => {
     return res
 }, error => {
     console.log('response error: ' + error) // for error debug
+    // 跳转到登陆页面
+    router.push("/")
     return Promise.reject(error)
 })
 
